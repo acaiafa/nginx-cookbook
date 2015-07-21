f6431eca78f2236d50b03b42d6e1f130068518a2
Anthony Caiafa <2600.ac@gmail.com>
2015-07-21 00:16:18 -0400

    removing proc_init and moving to service.options -- thanks noah

--- libraries/nginx_service.rb
+++ libraries/nginx_service.rb
@@ -29,7 +29,7 @@ class Chef::Resource::NginxService < Chef::Resource
   # @see: default nginx.conf configuration file
   attribute(:source, kind_of: String, default: "nginx.conf.erb")
   attribute(:worker_processes, kind_of: Integer, default: 4)
-  attribute(:pid_file, kind_of: String, default: '/var/run/nginx.pid')
+  attribute(:pid_file, kind_of: String, default: '/run/nginx/nginx.pid')
   attribute(:worker_connections, kind_of: Integer, default: '1024')
   attribute(:sendfile, equal_to: %w{on off}, default: 'on')
   attribute(:tcp_nopush, equal_to: %w{on off}, default: 'on')
@@ -80,6 +80,14 @@ class Chef::Provider::NginxService < Chef::Provider
         action :create
       end
 
+      # Create pid dir
+      directory "/run/nginx" do 
+        owner "www-data"
+        group "root"
+        mode "0755"
+        action :create
+      end
+
       # Create primary nginx directory
       directory "#{new_resource.instance} :create /etc/nginx" do
         path "/etc/nginx"
@@ -112,11 +120,6 @@ class Chef::Provider::NginxService < Chef::Provider
         )
         action :create
       end
-
-      # override templates for nginx specific options
-      poise_service "nginx" do
-          options ":#{proc_init}", template: "nginx:#{proc_init}.erb"
-      end
     end
     super
   end
@@ -124,8 +127,11 @@ class Chef::Provider::NginxService < Chef::Provider
   def service_options(service)
     service.service_name('nginx')
     service.command('/usr/sbin/nginx')
-    service.environment({'PID' => '/var/run/nginx/nginx.pid'})
+    service.directory('/run/nginx')
+    service.environment({'PID' => '/run/nginx/nginx.pid'})
     service.user(platform_user)
     service.restart_on_update(true)
+    service.options :systemd, template: "nginx:systemd.erb"
+    service.options :upstart, template: "nginx:upstart.erb"
   end
 end
